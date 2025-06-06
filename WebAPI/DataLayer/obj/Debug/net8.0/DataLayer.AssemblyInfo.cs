5047ab8583cfd33faa4329416c3f40ae19f2f14c
Evgeni Genchev <120590298+EvgeniGenchev07@users.noreply.github.com>
2025-06-06 17:46:47 +0300

    cleanup

--- WebAPI/DataLayer/obj/Debug/net8.0/DataLayer.AssemblyInfo.cs
+++ WebAPI/DataLayer/obj/Debug/net8.0/DataLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61c5bc75217529c896852ef266d8d1752c1562e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21cefeee781c5fc2d9fa86f51565bb9c0d429850")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
